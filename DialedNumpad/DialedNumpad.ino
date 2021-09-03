4165da1001239f50a577591c152312c517d6c337
Nikita Tomilov <programmer174@icloud.com>
2021-09-03 20:55:53 +0300

    code: copypaste example of multiple key press

--- DialedNumpad/DialedNumpad.ino
+++ DialedNumpad/DialedNumpad.ino
@@ -1,32 +1,77 @@
+/* @file CustomKeypad.pde
+|| @version 1.0
+|| @author Alexander Brevig
+|| @contact alexanderbrevig@gmail.com
+||
+|| @description
+|| | Demonstrates changing the keypad size and key values.
+|| #
+*/
 #include <Keypad.h>
 
-const byte ROWS = 4; //four rows
-const byte COLS = 6; //four columns
-//define the cymbols on the buttons of the keypads
+const byte ROWS = 4;
+const byte COLS = 6;
 char hexaKeys[ROWS][COLS] = {
-  {'0','1','2','3', 'A', 'B'},
-  {'4','5','6','7', 'C', 'D'},
-  {'8','9','E','F', 'G', 'H'},
-  {'I', 'J', 'K', 'L', 'M', 'N'}
+  //6  7   4   5   2   3
+  {'a','b','c','d','e','f'}, //10
+  {'g','h','i','j','k','l'}, //16
+  {'m','n','o','p','q','r'}, //9
+  {'s','t','u','v','w','x'}  //8
 };
-byte rowPins[ROWS] =  {10, 16, 9, 8}; //connect to the row pinouts of the keypad
-byte colPins[COLS] = {6, 7, 4, 5, 2, 3};//connect to the column pinouts of the keypad
+byte rowPins[ROWS] =  {10, 16, 9, 8};
+byte colPins[COLS] = {6, 7, /*4*/4, 5, 2, /*3*/3};
+
 
 //initialize an instance of class NewKeypad
-Keypad customKeypad = Keypad( makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS); 
+Keypad kpd = Keypad( makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS); 
+
+unsigned long loopCount;
+unsigned long startTime;
+String msg;
 
 void setup(){
   Serial.begin(115200);
   while (!Serial) {};
   Serial.println("start ok");
+  loopCount = 0;
+  startTime = millis();
+  msg = "";
 }
   
-void loop(){
- // Serial.println("test");
- // delay(1000);
-  char customKey = customKeypad.getKey();
-  
-  if (customKey){
-    Serial.println(customKey);
-  }
-}
+void loop() {
+    loopCount++;
+    if ( (millis()-startTime)>5000 ) {
+        Serial.print("Average loops per second = ");
+        Serial.println(loopCount/5);
+        startTime = millis();
+        loopCount = 0;
+    }
+
+    // Fills kpd.key[ ] array with up-to 10 active keys.
+    // Returns true if there are ANY active keys.
+    if (kpd.getKeys())
+    {
+        for (int i=0; i<LIST_MAX; i++)   // Scan the whole key list.
+        {
+            if ( kpd.key[i].stateChanged )   // Only find keys that have changed state.
+            {
+                switch (kpd.key[i].kstate) {  // Report active key state : IDLE, PRESSED, HOLD, or RELEASED
+                    case PRESSED:
+                    msg = " PRESSED.";
+                break;
+                    case HOLD:
+                    msg = " HOLD.";
+                break;
+                    case RELEASED:
+                    msg = " RELEASED.";
+                break;
+                    case IDLE:
+                    msg = " IDLE.";
+                }
+                Serial.print("Key ");
+                Serial.print(kpd.key[i].kchar);
+                Serial.println(msg);
+            }
+        }
+    }
+}  // End loop
