2577b57a997dbba4562b649b82d4e7bd30d09e15
Nikita Tomilov <programmer174@icloud.com>
2021-09-03 20:09:42 +0300

    code: test based on Keypad.h lib

--- DialedNumpad/DialedNumpad.ino
+++ DialedNumpad/DialedNumpad.ino
@@ -1,9 +1,32 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <Keypad.h>
 
+const byte ROWS = 4; //four rows
+const byte COLS = 6; //four columns
+//define the cymbols on the buttons of the keypads
+char hexaKeys[ROWS][COLS] = {
+  {'0','1','2','3', 'A', 'B'},
+  {'4','5','6','7', 'C', 'D'},
+  {'8','9','E','F', 'G', 'H'},
+  {'I', 'J', 'K', 'L', 'M', 'N'}
+};
+byte rowPins[ROWS] =  {10, 16, 9, 8}; //connect to the row pinouts of the keypad
+byte colPins[COLS] = {6, 7, 4, 5, 2, 3};//connect to the column pinouts of the keypad
+
+//initialize an instance of class NewKeypad
+Keypad customKeypad = Keypad( makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS); 
+
+void setup(){
+  Serial.begin(115200);
+  while (!Serial) {};
+  Serial.println("start ok");
 }
-
-void loop() {
-  // put your main code here, to run repeatedly:
-
+  
+void loop(){
+ // Serial.println("test");
+ // delay(1000);
+  char customKey = customKeypad.getKey();
+  
+  if (customKey){
+    Serial.println(customKey);
+  }
 }
