588d48371b4114e99f9dbf7d1003415fdbbc7782
Nikita Tomilov <programmer174@icloud.com>
2021-09-06 12:37:00 +0300

    KeycodesDef.h: backspace/delete

--- DialedNumpad/KeycodesDef.h
+++ DialedNumpad/KeycodesDef.h
@@ -7,7 +7,7 @@ KeyEvent* encEvents[3] = { encRotateLeftEvents, encRotateRightEvents, encPressEv
 
 KeyEvent keyEventsLayer0[6][4] = { 
   { {K, KEY_F13}, {K, KEY_F14}, {K, KEY_F15}, {K, KEY_F16} },
-  { {K, KEY_NUM_LOCK}, {K, KEY_LEFT}, {K, KEY_RIGHT}, {K, KEYPAD_ADD} },
+  { {K, KEY_NUM_LOCK}, {K, KEY_BACKSPACE}, {K, KEY_DELETE}, {K, KEYPAD_ADD} },
   { {K, KEYPAD_7}, {K, KEYPAD_8}, {K, KEYPAD_9}, {K, KEYPAD_SUBTRACT} },
   { {K, KEYPAD_4}, {K, KEYPAD_5}, {K, KEYPAD_6}, {K, KEYPAD_MULTIPLY} },
   { {K, KEYPAD_1}, {K, KEYPAD_2}, {K, KEYPAD_3}, {K, KEYPAD_DIVIDE} },
